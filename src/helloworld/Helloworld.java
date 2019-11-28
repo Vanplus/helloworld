f6031ab5fab607270baf6786591614f24e29a77d
Administrator <Administrator@SC-201907312026>
2019-11-28 14:46:14 +0800

    test

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -4,7 +4,7 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld");
+		System.out.println("HelloWorld!Ê²Ã´ÊÇÂÔÎ¢ÐÞ¸Ä£¡");
 	}
 
 }
